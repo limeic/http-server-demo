684f1bb455feb45464a306a855c616514bde751c
Allen.Wu <allenlikeu@gmail.com>
2017-11-22 16:14:45 +0800

    Allen.Wu: complete oneway request

--- build/routes/index.js
+++ build/routes/index.js
@@ -4,15 +4,67 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _http = require('http');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// 处理首页信息
 
 var router = new _express.Router();
 
 // index默认路由
-// 处理首页信息
-
 router.get('/', function (req, res, next) {
     res.render('index', {});
+});
+
+// ping Get测试
+router.get('/ping', function (req, res, next) {
+    var data = req.query.data;
+    console.log("recevied ping data: ", data);
+    res.send(data);
+    res.end();
+});
+
+// ping Post测试
+router.post('/ping', function (req, res, next) {
+    var data = req.body;
+    console.log("received ping data: ", data);
+    res.send(data);
+    res.end();
+});
+
+// broadcast 测试
+router.post('/broadcast', function (req, res, next) {
+    var data = req.body;
+    console.log("received broadcast data: ", data);
+
+    var resp = {
+        "code": 200,
+        "msg": "success"
+    };
+
+    // 判断userId是否存在
+    if (!data.userId) {
+        console.error("no userId exists in body: ", data.userId);
+        resp.code = 500;
+        resp.msg = "no userId exists!";
+        res.send(resp);
+        return;
+    }
+
+    // 判断data是否存在
+    if (!data.data) {
+        console.error("no data exists in body: ", data.data);
+        resp.code = 500;
+        resp.msg = "no data exists!";
+        res.send(resp);
+        return;
+    }
+
+    //TODO: 通知到gateway broadcast server
+
+    res.send(resp);
+    res.end();
 });
 
 module.exports = router;
--- source/routes/index.js
+++ source/routes/index.js
@@ -1,12 +1,63 @@
 // 处理首页信息
 
 import express, {Router} from 'express';
+import { request } from 'http';
 
 let router = new Router();
 
 // index默认路由
 router.get('/', (req, res, next) => {
     res.render('index', {});
+});
+
+// ping Get测试
+router.get('/ping', (req, res, next) => {
+    let data = req.query.data;
+    console.log("recevied ping data: ", data);
+    res.send(data);
+    res.end();
+});
+
+// ping Post测试
+router.post('/ping', (req, res, next) => {
+    let data = req.body;
+    console.log("received ping data: ", data);
+    res.send(data);
+    res.end();
+});
+
+// broadcast 测试
+router.post('/broadcast', (req, res, next) => {
+    let data = req.body;
+    console.log("received broadcast data: ", data);
+
+    let resp = {
+        "code": 200,
+        "msg": "success"
+    };
+
+    // 判断userId是否存在
+    if ( !data.userId ) {
+        console.error("no userId exists in body: ", data.userId);
+        resp.code = 500;
+        resp.msg = "no userId exists!";
+        res.send(resp);
+        return ;
+    }
+
+    // 判断data是否存在
+    if ( !data.data ) {
+        console.error("no data exists in body: ", data.data);
+        resp.code = 500;
+        resp.msg = "no data exists!";
+        res.send(resp);
+        return ;
+    }
+
+    //TODO: 通知到gateway broadcast server
+
+    res.send(resp);
+    res.end();
 });
 
 module.exports = router;
