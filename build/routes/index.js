f54e3f2e929d3add518878097c13400f91f28db8
Team LimeIC <allen.wu@limeic.com>
2018-05-30 12:43:11 +0800

    Allen.Wu: 修正广播测试接口

--- build/routes/index.js
+++ build/routes/index.js
@@ -10,13 +10,15 @@ var _request2 = _interopRequireDefault(_request);
 
 var _util = require('util');
 
+var _dns = require('dns');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// 处理首页信息
 
 var router = new _express.Router();
 
 // index默认路由
-// 处理首页信息
-
 router.get('/', function (req, res, next) {
     res.render('index', {});
 });
@@ -39,6 +41,7 @@ router.post('/ping', function (req, res, next) {
 
 // broadcast 测试
 router.post('/broadcast', function (req, res, next) {
+
     var data = req.body;
     console.log("received broadcast data: ", data);
 
@@ -47,8 +50,8 @@ router.post('/broadcast', function (req, res, next) {
         "msg": "success"
     };
 
-    if (!data.appId) {
-        console.error("no appId exists in body: ", data.appId);
+    if (!data.appKey) {
+        console.error("no appId exists in body: ", data.appKey);
         resp.code = 500;
         resp.msg = "no appId exists!";
         res.send(resp);
@@ -56,8 +59,8 @@ router.post('/broadcast', function (req, res, next) {
     }
 
     // 判断userId是否存在
-    if (!data.toUserIdList) {
-        console.error("no toUserIdList exists in body: ", data.userId);
+    if (!data.userIds) {
+        console.error("no userIds exists in body: ", data.userIds);
         resp.code = 500;
         resp.msg = "no userId exists!";
         res.send(resp);
@@ -73,25 +76,42 @@ router.post('/broadcast', function (req, res, next) {
         return;
     }
 
-    // 通知到gateway broadcast server
-    var broadcastUrl = data.gatewayDomain + "/api/data/broadcast";
-    console.log("broadcastUrl: ", broadcastUrl);
-    var param = {
-        appKey: data.appId,
-        toUserIdList: data.toUserIdList,
-        data: JSON.stringify(data.data)
-    };
-    console.log("send param: ", param);
+    //  从Lemon RESTful API中获取appId对应的domain列表。每个domain列表都需要发送请求
+    // 测试地址
     (0, _request2.default)({
-        url: broadcastUrl,
+        url: "http://test.api.limeic.com/rest/connectors/http",
         method: "POST",
-        json: param
+        json: {
+            appKey: data.appKey,
+            userIds: data.userIds
+        }
     }, function (err, resp, body) {
+
         if (!!err) {
-            console.log("broadcast err: ", err);
+            console.log("fetch connectors err: ", err);
             return;
         }
-        console.log("broadcast resp body: ", body);
+
+        var content = resp.body;
+        if (content.status != "ok") {
+            console.log("fetch connectors failed. resp: ", content);
+            return;
+        }
+
+        // 发送广播请求
+        content.connectors.forEach(function (connector) {
+            (0, _request2.default)({
+                url: "http://" + connector.domain + "/api/data/broadcast",
+                method: "POST",
+                json: data
+            }, function (err, resp, body) {
+                if (!!err) {
+                    console.log("broadcast err: ", err);
+                    return;
+                }
+                console.log("broadcast resp body: ", body);
+            });
+        });
     });
 
     res.send(resp);
--- source/routes/index.js
+++ source/routes/index.js
@@ -3,6 +3,7 @@
 import express, {Router} from 'express';
 import Request from 'request';
 import { log } from 'util';
+import { BADRESP } from 'dns';
 
 let router = new Router();
 
@@ -29,6 +30,7 @@ router.post('/ping', (req, res, next) => {
 
 // broadcast 测试
 router.post('/broadcast', (req, res, next) => {
+
     let data = req.body;
     console.log("received broadcast data: ", data);
 
@@ -37,8 +39,8 @@ router.post('/broadcast', (req, res, next) => {
         "msg": "success"
     };
 
-    if ( !data.appId ) {
-        console.error("no appId exists in body: ", data.appId);
+    if ( !data.appKey ) {
+        console.error("no appId exists in body: ", data.appKey);
         resp.code = 500;
         resp.msg = "no appId exists!";
         res.send(resp);
@@ -46,8 +48,8 @@ router.post('/broadcast', (req, res, next) => {
     }
 
     // 判断userId是否存在
-    if ( !data.toUserIdList ) {
-        console.error("no toUserIdList exists in body: ", data.userId);
+    if ( !data.userIds ) {
+        console.error("no userIds exists in body: ", data.userIds);
         resp.code = 500;
         resp.msg = "no userId exists!";
         res.send(resp);
@@ -63,25 +65,43 @@ router.post('/broadcast', (req, res, next) => {
         return ;
     }
 
-    // 通知到gateway broadcast server
-    let broadcastUrl = data.gatewayDomain + "/api/data/broadcast";
-    console.log("broadcastUrl: ", broadcastUrl);
-    let param = {
-        appKey: data.appId,
-        toUserIdList: data.toUserIdList,
-        data: JSON.stringify(data.data),
-    };
-    console.log("send param: ", param);
+    //  从Lemon RESTful API中获取appId对应的domain列表。每个domain列表都需要发送请求
+    // 测试地址
     Request({
-        url: broadcastUrl,
+        url: "http://test.api.limeic.com/rest/connectors/http",
         method: "POST",
-        json: param
+        json: {
+            appKey: data.appKey,
+            userIds: data.userIds,
+        }
     }, (err, resp, body) => {
+
         if ( !!err ) {
-            console.log("broadcast err: ", err);
+            console.log("fetch connectors err: ", err);
             return ;
-        }    
-        console.log("broadcast resp body: ", body);
+        }
+
+        const content = resp.body;
+        if ( content.status != "ok" ) {
+            console.log("fetch connectors failed. resp: ", content);
+            return ;
+        }
+    
+        // 发送广播请求
+        content.connectors.forEach(connector => {            
+            Request({
+                url: "http://" + connector.domain + "/api/data/broadcast",
+                method: "POST",
+                json: data
+            }, (err, resp, body) => {
+                if ( !!err ) {
+                    console.log("broadcast err: ", err);
+                    return ;
+                }    
+                console.log("broadcast resp body: ", body);
+            });
+        });
+
     });
 
     res.send(resp);
